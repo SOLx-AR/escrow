3eea16338709d2c74e837cb1672eefb1e045a31b
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2025-09-09 20:11:49 -0300

    refund

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -16,22 +16,19 @@ pub mod escrow {
     // y define que moneda y cantidad quiere recivir a cambio
     pub fn make(ctx: Context<Make>, seed: u64, deposit: u64, receive:u64) -> Result<()> {
         ctx.accounts.deposit(deposit);
-        ctx.accounts.save_escrow(seed, receive, &ctx.bumps)?;
-        Ok(())
+        ctx.accounts.save_escrow(seed, receive, &ctx.bumps)
     }
 
     // otro usuario acepta el escrow y deposita la cantidad y moneda requerida
     // el usuario que crea el escrow recibe la cantidad y moneda que definio
     pub fn take(ctx: Context<Take>) -> Result<()> {
-
-        Ok(())
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close()
     }
-
-   /* 
+   
     // el usuario creado cancela el escrow
     pub fn refund(ctx: Context<Refund>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+        ctx.accounts.refund_and_close_vault()
     }
-    */
+    
 }
